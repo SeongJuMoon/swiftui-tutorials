fbeefb2ba4d7ab1d9cadc3cb3adb5e71d653748f
SeongJuMoon <arcuer.dev@gmail.com>
2019-06-07 17:48:27 +0900

    Initial Commit

---
+++ swiftui-tutorials/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  swiftui-tutorials
+//
+//  Created by 문성주 on 07/06/2019.
+//  Copyright © 2019 seongju. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
